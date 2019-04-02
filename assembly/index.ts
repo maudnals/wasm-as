a24bc50922818b3810dc1a48de99e5ec95127e37
maudnals <maud.nalpas@gmail.com>
2019-04-02 08:40:37 +0200

    refactor(): removed unused JS

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,9 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function desaturate(arr: TypedArray<u16>): TypedArray<u16> {
+  // const a = new TypedArray<u16>;
+  // TypedArray<u16>.
+  // return new TypedArray<u16>;
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 console.log('coucou');
-import all from './build/optimized.wasm';
 import { add } from './build/optimized.wasm';
 
-console.log('all', all);
 console.log('add', add(1, 2));
