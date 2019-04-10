7e04ffd9e435112a22776e48996cdb5754264af0
maudnals <maud.nalpas@gmail.com>
2019-04-10 13:28:35 +0200

    refactor(): better folder struct

--- src/01-basic.ts
+++ src/01-basic.ts
@@ -3,7 +3,7 @@ import {
   getInput1Value,
   getInput2Value,
   sumDivEl
-} from './html.helper';
+} from './utils/html.utils';
 import { add } from './wasm/build/optimized2.wasm';
 
 display(sumDivEl, add(getInput1Value(), getInput2Value()));
--- src/initialize.ts
+++ src/initialize.ts
@@ -1,5 +1,5 @@
-import { setInput1Value, setInput2Value } from './html.helper';
-import { getRandom1To10 } from './maths.helper';
+import { setInput1Value, setInput2Value } from './utils/html.utils';
+import { getRandom1To10 } from './utils/maths.utils';
 
 setInput1Value(getRandom1To10());
 setInput2Value(getRandom1To10());
