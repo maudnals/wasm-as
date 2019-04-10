57f985589af1c3a21c887ab14eb02ee1dc339393
maudnals <maud.nalpas@gmail.com>
2019-04-10 14:52:54 +0200

    feat(dom): added types

--- src/utils/dom.utils.ts
+++ src/utils/dom.utils.ts
@@ -2,10 +2,12 @@
 
 export const input1: HTMLInputElement = document.getElementById('input1');
 export const input2: HTMLInputElement = document.getElementById('input2');
-export const setInput1Value = value => (input1.value = value);
-export const setInput2Value = value => (input2.value = value);
-export const getInput1Value = () => input1.value;
-export const getInput2Value = () => input2.value;
+export const setInput1Value = (value: number) =>
+  (input1.value = <string>(<unknown>value));
+export const setInput2Value = (value: number) =>
+  (input2.value = <string>(<unknown>value));
+export const getInput1Value = (): string => input1.value;
+export const getInput2Value = (): string => input2.value;
 
 export const sumDivEl: HTMLElement = document.getElementById('sumDiv');
 
@@ -13,6 +15,6 @@ export const editedImageWrapperEl: HTMLElement = document.getElementById(
   'editedImageWrapper'
 );
 
-export const display = (el, innerHTML): void => {
+export const display = (el: HTMLElement, innerHTML: string): void => {
   el.innerHTML = innerHTML;
 };
