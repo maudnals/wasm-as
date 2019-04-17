1a98d1751d6c545cf9dcde03adeb7a3495b2dada
maudnals <maud.nalpas@gmail.com>
2019-04-17 11:56:30 +0200

    feat(imgOperation): trigger on button click

--- src/02-imgOperation.ts
+++ src/02-imgOperation.ts
@@ -1,31 +1,31 @@
 import loader from 'assemblyscript/lib/loader';
 import { displayImage, getImgDataArray } from './utils/canvas.utils';
-import { editedImageWrapperEl } from './utils/dom.utils';
+import { newImgWrapperEl, btn } from './utils/dom.utils';
 
-fetch('doubleArray.0aceb6d0.wasm')
-  .then(bytes => bytes.arrayBuffer())
-  .then(buffer => {
-    const wasmModule = loader.instantiateBuffer(<Uint8Array>buffer, {
-      env: {}
+btn.addEventListener('click', () => {
+  fetch('doubleArray.0aceb6d0.wasm')
+    .then(bytes => bytes.arrayBuffer())
+    .then(buffer => {
+      const arrData = getImgDataArray('img');
+
+      const wasmModule = loader.instantiateBuffer(<Uint8Array>buffer, {
+        env: {}
+      });
+      // pointer to memory location (in WASM context)
+      const ptr = wasmModule.newArray(new Int32Array(arrData));
+      // double all RGB values (separately but the operation is linear)
+      wasmModule.sum(ptr);
+      // access the processed array
+      const doubledArray = wasmModule.getArray(Int32Array, ptr);
+      // free memory in WASM context
+      wasmModule.freeArray(ptr);
+
+      displayImage(new Uint8ClampedArray(doubledArray), newImgWrapperEl);
+
+      console.log(arrData);
+      console.log('ptr:', ptr);
+      console.log('wasmModule:', wasmModule);
+      console.log('wasmModule.memory:', wasmModule.memory);
+      console.log('doubledArray:', doubledArray);
     });
-
-    const arrData = getImgDataArray('img');
-    console.log(arrData);
-
-    // pointer to memory location (in WASM context)
-    const ptr = wasmModule.newArray(new Int32Array(arrData));
-    // double all RGB values (separately but the operation is linear)
-    wasmModule.sum(ptr);
-    // access the processed array
-    const doubledArray = wasmModule.getArray(Int32Array, ptr);
-    // free memory in WASM context
-    wasmModule.freeArray(ptr);
-
-    displayImage(new Uint8ClampedArray(doubledArray), editedImageWrapperEl);
-
-    console.log('ptr:', ptr);
-    console.log('wasmModule:', wasmModule);
-    console.log('wasmModule.memory:', wasmModule.memory);
-    console.log('doubledArray', doubledArray);
-    console.log('doubledArray converted', new Uint8ClampedArray(doubledArray));
-  });
+});
